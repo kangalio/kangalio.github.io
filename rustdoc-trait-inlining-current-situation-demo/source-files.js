640987f79fac31632af30dd386f1d51e9d2602d5
kangalioo <jannik.a.schaper@web.de>
2020-07-07 11:09:52 +0200

    rustdoc-trait-inlining-current-situation-demo

---
+++ rustdoc-trait-inlining-current-situation-demo/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["test"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
---
+++ rustdoc-trait-inlining-current-situation-demo/test/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["DeviceA","A basic device. Implements the base functionality set, and then some"],["DeviceB","A futuristic device with only futuristic capabilities"],["DeviceC","This device has futuristic features, but also some backwards-compatibility"]],"trait":[["FunctionalitySet1",""],["FunctionalitySet2",""],["FunctionalitySet3",""]]});
